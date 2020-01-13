ca36ebb9a0e80bc9073ee4c79710b3f4caedc3e4
DavidGF <DavidGuFu@gmail.com>
2020-01-13 10:04:08 +0100

    nuevo mvc ejercicio3 iniciado php para usar

--- mcv1-2020-01-08/clases/Numeros.php
+++ mcv1-2020-01-08/clases/Numeros.php
@@ -4,10 +4,30 @@
 namespace clases;
 
 class Numeros {
-    function __construct($valores=[]) {
+    public function __construct($valores=[]) {
     $this->valores =$valores;
-    $this->setModa();
+    }
+    public function GetMedia(){
+    return $this->Media;}
+    public function GetModa(){
+    return $this->Moda;}
+    public function GetMediana(){
+    return $this->Mediana;}
+    public function GetDesviacion(){
+    return $this->Desviacion;
+    }
+    public function setMedia(){
+        $this->media= array_sum($this->getValores())/count($this->getValores());
         
     }
-
+    public function setModa(){
+        $salida= array_count_values($this->getValores());
+        $repeticionesMaximas=max($salida);
+        $this->moda=[];
+        foreach ($salida as $numeros=>$repes){
+            If($repes==$repeticionesMaximas){
+            $this->moda[]=$numeros;
+            }
+        }
+    }
 }
