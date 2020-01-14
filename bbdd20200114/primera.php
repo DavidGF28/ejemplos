e27e8a918b4033c193bd2741e8e7f293b7906d9b
DavidGF <DavidGuFu@gmail.com>
2020-01-14 10:47:45 +0100

    bases de datos inicio poo

---
+++ bbdd20200114/primera.php
@@ -0,0 +1,13 @@
+<?php
+//conexion
+$conexion = mysqli_connect("127.0.0.1", "root", "");
+var_dump($conexion);
+//selecionar DB
+$conexion->select_db("videoteca");
+var_dump($conexion);
+//preparar result
+$resultado=$conexion->query("select * from actor");
+var_dump($resultado);
+//ver result
+$salidaArray=$resultado->fetch_all();
+var_dump($salidaArray);
